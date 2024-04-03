373c47e71527166aa0a0d497f0a5bba92848dc38
Han Khang <s3986602@rmit.edu.vn>
2024-04-03 13:46:53 +0700

    add Customer class

---
+++ Customer.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+/**
+ * @author Han Duc Khang -  s3986602
+ */
+public class Customer {
+    private String id;
+    private String name;
+    private InsuranceCard insurancecard;
+    private List<Claim> claims;
+
+    public Customer(String id, String name, InsuranceCard insurancecard, List<Claim> claims) {
+        this.id = id;
+        this.name = name;
+        this.insurancecard = insurancecard;
+        this.claims = claims;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public InsuranceCard getInsurancecard() {
+        return insurancecard;
+    }
+
+    public List<Claim> getClaims() {
+        return claims;
+    }
+
+    //setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setInsurancecard(InsuranceCard insurancecard) {
+        this.insurancecard = insurancecard;
+    }
+
+    public void setClaims(List<Claim> claims) {
+        this.claims = claims;
+    }
+}
