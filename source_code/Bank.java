35572f1c831e16fd4b35422f6ad1d761bcb8bb51
Kuu <s3986602@rmit.edu.vn>
2024-04-06 13:33:05 +0700

    add bank
    modified Claim

---
+++ source_code/Bank.java
@@ -0,0 +1,40 @@
+package source_code;
+/**
+ * @Author Han Duc Khang - s3986602
+ */
+public class Bank {
+    private String bankName;
+    private String accountHolderName;
+    private String accountNumber;
+
+    public Bank(String bankName, String accountHolderName, String accountNumber) {
+        this.bankName = bankName;
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+    }
+
+    // Getters and setters
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
